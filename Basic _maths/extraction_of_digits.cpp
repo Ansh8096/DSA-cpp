12f45e49f210ad7c857b1adc2994472bc34ff77b
Ansh Verma <anshv8096@gmail.com>
2025-07-12 23:22:15 +0530

    extraction_of_digits.cpp

--- Basic _maths/extraction_of_digits.cpp
+++ Basic _maths/extraction_of_digits.cpp
@@ -1,1 +1,18 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+void explain_extraction_digits(auto n){
+    while (n>1)
+    {
+        auto lastdigit=n%10;
+        cout<<lastdigit<<endl;
+        n=n/10; 
+    }   
+}
+int main()
+{
+    int a;
+    cout<<"enter values: "<<endl;
+    cin>>a;  
+    explain_extraction_digits(a);  
+    return 0;
+}
