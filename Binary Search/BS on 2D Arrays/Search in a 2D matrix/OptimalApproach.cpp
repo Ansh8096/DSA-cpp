b1843c261cf52d883e1e7ef306da8a5580a39a17
Ansh Verma <anshv8096@gmail.com>
2025-12-02 23:48:22 +0530

    Implement optimal search in 2D matrix

---
+++ Binary Search/BS on 2D Arrays/Search in a 2D matrix/OptimalApproach.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+string search_in_2D_arr_OP(vector<vector<int>> &a, int n, int m, int tar)
+{
+  int low = 0, high = (n * m) - 1;
+  while (low <= high)
+  {
+    int mid = (low + high) >> 1;
+    int row = mid / m;
+    int col = mid % m;
+    if (a[row][col] == tar) return "true";
+    else if (a[row][col] < tar) low = mid + 1;
+    else high = mid - 1;
+  }
+  return "false";
+}
+int main(){
+  vector<vector<int>> matrix ={
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12}
+  };
+
+  int n = matrix.size();
+  int m = matrix[0].size();
+
+  int tar;
+  cout<<"enter the target value: "<<endl;
+  cin>>tar;
+
+  cout<<search_in_2D_arr_OP(matrix,n,m,tar);
+  return 0;
+}
